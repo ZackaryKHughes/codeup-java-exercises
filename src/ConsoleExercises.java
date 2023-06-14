250d6e64e0a3b44947dd147ca8660b0dfeecd250
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-06-14 14:00:35 -0500

    Finished exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f.", pi);
+        System.out.format("The value of pi is approximately %.2f.", pi);
 
 
         Scanner input = new Scanner(System.in);
